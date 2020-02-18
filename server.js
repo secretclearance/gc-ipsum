debe485d496b826ff5c7250984a9b539e95c0455
Clarence <secretclearance2020@gmail.com>
2020-02-18 00:35:28 +0000

    Added tweet route.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var serveStatic = require('serve-static');
 var application = express();
 
+const loremIpsum = require("lorem-ipsum").loremIpsum;
+
 var fs = require('fs');
 
 var array = fs.readFileSync('words.txt', 'utf8').split('\n');
@@ -10,10 +12,20 @@ application.get('/words', function (req, res) {
     res.send(JSON.stringify(array));
 })
 
+application.get('/tweet', function (req, res) {
+    let sentence = loremIpsum({
+        words: array,
+        units: "sentence",
+        count: 1,
+        format: "plain",
+    })
+
+    res.send(JSON.stringify({ response: `${sentence} #gcdigital` }));
+})
+
 var port = process.env.PORT || 5001;
 
 application.use(serveStatic(__dirname + "/dist"));
-
 
 application.listen(port);
 
